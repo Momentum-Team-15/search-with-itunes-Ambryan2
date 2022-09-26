ecb00afeb765ed418f8bc13fbbdd48a490c9a1ba
Your Name <alobryan2@gmail.com>
2022-09-26 19:47:09 -0400

    skelton should be done now to add the meat

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 //temperary arrays I will use to find the artist in itunes database
-let desiredArt = [];
-let artistInfo = [];
+let desiredArt = []; //this will contain the artist written in search
+let artistResults = []; //this will hold the info of artist from apple array
 
 //this function filters through desiredArt function and then finds in another array that has matching information
 function finder(array) {
     for (let j = 0; j < array.length; j++) { 
         for (let i = 0; i < newCust.length; i++) {
             if (newCust[i].name.first === array[j]) { //newCust data should be replaced with array information I would need 
-                artistInfo.push(newCust[i]); //this all the info of peoples names selected
+                artistResults.push(newCust[i]); //this all the info of peoples names selected
             }
         }
     }
+}
+
+let resultGrid = document.querySelector('#musicContainer');
+
+//fake array for test
+let fakeArr = ['bob','sally','marry','christ','jet','truck','tank','chair','pops','dork']
+//function that build the grid on the bottom
+function searchGrid(results) {
+    let resultCont = document.createElement('div');
+    for (let i = 0; i < 15;i++){   
+    let indivResult = document.createElement('div');
+    let albumCov = document.createElement('img');
+    let songTitle = document.createElement('p');
+    let bandName = document.createElement('h2');
+
+    //this is where giv albumCOv, songTitle and BandName information
+    bandName.innerText = results[i]
+    //adding everything to result div
+    indivResult.classList.add('songOption');
+    indivResult.appendChild(albumCov);
+    indivResult.appendChild(songTitle);
+    indivResult.appendChild(bandName);
+    console.log(indivResult)
+    resultCont.appendChild(indivResult);
+}
+//adding everything to container in html 
+resultCont.classList.add('gridStyle')
+resultGrid.appendChild(resultCont);
 }
 
 //to collect input from the search bar and the search button
@@ -21,6 +49,7 @@ let artist = document.querySelector("#artistBar");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     desiredArt = artist.value;
+    searchGrid(fakeArr);
     //need a function that will clear the results already displayed
 
     //need a function that will build the music results on bottom
