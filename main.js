6a88c58d179ea8fed0661ecead017a875230e4ba
Your Name <alobryan2@gmail.com>
2022-09-26 20:04:57 -0400

    progress on grid creation

--- main.js
+++ main.js
@@ -4,52 +4,74 @@ let artistResults = []; //this will hold the info of artist from apple array
 
 //this function filters through desiredArt function and then finds in another array that has matching information
 function finder(array) {
-    for (let j = 0; j < array.length; j++) { 
+    for (let j = 0; j < array.length; j++) {
         for (let i = 0; i < newCust.length; i++) {
-            if (newCust[i].name.first === array[j]) { //newCust data should be replaced with array information I would need 
+            if (newCust[i].name.first === array[j]) {
+                //newCust data should be replaced with array information I would need
                 artistResults.push(newCust[i]); //this all the info of peoples names selected
             }
         }
     }
 }
 
-let resultGrid = document.querySelector('#musicContainer');
+let resultGrid = document.querySelector("#musicContainer");
 
 //fake array for test
-let fakeArr = ['bob','sally','marry','christ','jet','truck','tank','chair','pops','dork']
+let fakeArr = [
+    "bob",
+    "sally",
+    "marry",
+    "christ",
+    "jet",
+    "truck",
+    "tank",
+    "chair",
+    "pops",
+    "dork",
+];
 //function that build the grid on the bottom
 function searchGrid(results) {
-    let resultCont = document.createElement('div');
-    for (let i = 0; i < 15;i++){   
-    let indivResult = document.createElement('div');
-    let albumCov = document.createElement('img');
-    let songTitle = document.createElement('p');
-    let bandName = document.createElement('h2');
+    let resultCont = document.createElement("div");
+    for (let i = 0; i < 15; i++) {
+        let indivResult = document.createElement("div");
+        let albumCov = document.createElement("img");
+        let songTitle = document.createElement("p");
+        let bandName = document.createElement("h2");
 
-    //this is where giv albumCOv, songTitle and BandName information
-    bandName.innerText = results[i]
-    //adding everything to result div
-    indivResult.classList.add('songOption');
-    indivResult.appendChild(albumCov);
-    indivResult.appendChild(songTitle);
-    indivResult.appendChild(bandName);
-    console.log(indivResult)
-    resultCont.appendChild(indivResult);
-}
-//adding everything to container in html 
-resultCont.classList.add('gridStyle')
-resultGrid.appendChild(resultCont);
+        //this is where giv albumCOv, songTitle and BandName information
+        bandName.innerText = results[i];
+        //adding everything to result div
+        indivResult.classList.add("songOption");
+        indivResult.appendChild(albumCov);
+        indivResult.appendChild(songTitle);
+        indivResult.appendChild(bandName);
+        resultCont.appendChild(indivResult);
+    }
+    //adding everything to container in html
+    resultCont.classList.add("gridStyle");
+    resultGrid.appendChild(resultCont);
 }
 
 //to collect input from the search bar and the search button
 let form = document.querySelector("#musicForm");
 let artist = document.querySelector("#artistBar");
 
+//function meant to hide anything in the grid
+function emptyGrid(container) {
+    let removeResults = container.querySelectorAll(".gridStyle");
+    for (let destroy of removeResults) {
+        container.removeChild(destroy);
+        console.log('grid was destroyed')
+    }
+}
 //when search is clicked this is what results
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     desiredArt = artist.value;
+    emptyGrid(resultGrid);
+    //this is a test in making grid
     searchGrid(fakeArr);
+
     //need a function that will clear the results already displayed
 
     //need a function that will build the music results on bottom
@@ -58,5 +80,4 @@ form.addEventListener("submit", (event) => {
 
     //also will need a function that will build the song player on top
 
-    console.log(desiredArt);
 });
